9d5832e3dae07c3deb9fc9c94a82b4e7e4afa851
Atai <58735260+slyrackgo@users.noreply.github.com>
2023-05-02 21:33:08 +0200

    Update project.sol

--- project.sol
+++ project.sol
@@ -1,56 +1,81 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-//Simple auction made for beneficiary
-contract simpleDonationg{
-    // paramaters of auction
-    address payable public beneficiary; //The one who selling sth in the auction
-    uint auctionEndTime; 
+pragma solidity ^0.8.0;
 
-    //Current state of the auctionEndTime
-    address public CurrentAddress;
-    uint public AmountDonated;
-    mapping(address => uint) public pendingReturns; //mapping where you put the address and get the integer as an output
-    bool ended = false;
-    event HighestBidIncrease(address bidder, uint amount);//Action when the bidder puts his amount
-    event AuctionEnded(address winner, uint amount); //Action when the winner win with certain amount of money
-    constructor(uint _biddingTime, address payable _beneficiary){//constructor is a function that is to initialize state variables in a contract
-        beneficiary = _beneficiary;
-        auctionEndTime = block.timestamp + _biddingTime;
+contract StartupInvestment {
+    address payable public startup;
+    address payable public investor;
+    uint public fundingGoal;
+    uint public investmentDeadline;
+    uint public amountRaised;
+    mapping(address => uint) public balanceOf;
+    bool public fundingGoalReached = false;
+    bool public investmentClosed = false;
+
+    event FundingGoalReached(address recipient, uint totalAmountRaised);
+    event InvestmentReceived(address backer, uint amount, bool isContribution);
+    event InvestmentRefunded(address backer, uint amount);
+
+    /* Constructor */
+    constructor(
+        address payable startupAddress,
+        uint fundingGoalInEthers,
+        uint investmentPeriodInDays
+    ) {
+        startup = startupAddress;
+        fundingGoal = fundingGoalInEthers * 1 ether;
+        investmentDeadline = block.timestamp + (investmentPeriodInDays * 1 days);
     }
 
-    function bid()public payable{
-        if(block.timestamp > auctionEndTime){
-            revert("The auction has finished.");// function that stops the function where it is
-        }
-        if(msg.value <= AmountDonated){
-            revert("There is already higher or equal bid.");
-        }
-        if(AmountDonated != 0){
-            pendingReturns[CurrentAddress] += AmountDonated;
-        }
-        AmountDonated = msg.value;
-        emit HighestBidIncrease(msg.sender, msg.value);//Emit  stores the arguments passed in transaction logs
+    /* The function that investors will call to invest in the startup */
+    function invest() public payable {
+        require(!investmentClosed);
+        require(block.timestamp <= investmentDeadline);
+        require(msg.value > 0);
+
+        balanceOf[msg.sender] += msg.value;
+        amountRaised += msg.value;
+        emit InvestmentReceived(msg.sender, msg.value, true);
     }
-    function withdraw()public returns (bool){
-        uint amount = pendingReturns[msg.sender];
-        if(amount > 0){
-            pendingReturns[msg.sender] = 0;
-            if(!payable(msg.sender).send(amount)){
-                pendingReturns[msg.sender] = amount;
-                return false;
-            }
-        }
-        return true;
+
+    /* The function that the startup will call to withdraw funds after the investment period has ended */
+    function withdraw() public {
+        require(investmentClosed);
+        require(fundingGoalReached);
+        require(msg.sender == startup);
+
+        startup.transfer(amountRaised);
     }
-    function auctionEnd()public{
-        if(block.timestamp < auctionEndTime){
-            revert("The auction has not ended yet.");
+
+    /* The function that investors will call to request a refund if the funding goal has not been reached */
+    function refund() public {
+        require(investmentClosed);
+        require(!fundingGoalReached);
+        require(balanceOf[msg.sender] > 0);
+
+        uint amountToRefund = balanceOf[msg.sender];
+        balanceOf[msg.sender] = 0;
+        msg.sender.transfer(amountToRefund);
+        emit InvestmentRefunded(msg.sender, amountToRefund);
+    }
+
+    /* The function that will close the investment period and distribute funds to the startup if the funding goal has been reached */
+    function close() public {
+        require(!investmentClosed);
+        if (amountRaised >= fundingGoal) {
+            fundingGoalReached = true;
+            emit FundingGoalReached(startup, amountRaised);
+            startup.transfer(amountRaised);
+        } else {
+            fundingGoalReached = false;
         }
-        if(ended){
-            revert("The function - auctionEnded, has been already called.");
+        investmentClosed = true;
+    }
+
+    /* The function that will return the time remaining until the investment period ends */
+    function getTimeRemaining() public view returns (uint) {
+        if (block.timestamp >= investmentDeadline) {
+            return 0;
+        } else {
+            return investmentDeadline - block.timestamp;
         }
-        ended = true;
-        emit AuctionEnded(CurrentAddress, AmountDonated);
-        beneficiary.transfer(AmountDonated); 
     }
 }
